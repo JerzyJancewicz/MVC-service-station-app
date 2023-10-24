8790dcfee29d6c42b93850b8a98d8524ae44677d
JerzyJancewicz <jancewiczjerzy@gmail.com>
2023-10-24 18:20:08 +0200

    Created db model which i have to implement

---
+++ ServiceStation.Domain/Entities/Services/ReplaceTyres.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStation.Domain.Entities.Details;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceStation.Domain.Entities.Services
+{
+    public class ReplaceTyres
+    {
+        public required int Id { get; set; }
+        public string ServiceName { get;private set; } = "zmiana-opon";
+        public string? Description { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public ContactDetails contactDetails { get; set; } = default!;
+        public string EncodedServiceName { get; private set; } = default!;
+
+        public void EncodeName() => EncodedServiceName = ServiceName.ToLower().Replace(" ","-");
+    }
+}
---
+++ ServiceStation.Infrastructure/Presistance/ServiceStationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceStation.Domain.Entities.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceStation.Infrastructure.Presistance
+{
+    public class ServiceStationDbContext : DbContext
+    {
+        public DbSet<ReplaceTyres> replaceTyres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Default");
+        }
+    }
+}
